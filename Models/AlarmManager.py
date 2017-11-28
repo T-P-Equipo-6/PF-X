22926ba0dd19ecc627ca81932e7b960214bec571
Alejandro Mendoza <zippi180@gmail.com>
2017-11-28 17:57:47 -0600

    Updated variable names and removed 'Magic Numbers'

--- Models/AlarmManager.py
+++ Models/AlarmManager.py
@@ -1,41 +1,43 @@
 class AlarmManager():
     def __init__(self, event_handler=None):
-        self.__sensor1 = None
-        self.__actual_sensor1 = None
-        self.__sensor1_values = []
-        self.__actual_sensor1_values = []
+        self.__perimeter = None
+        self.__actual_perimeter = None
+        self.__perimeter_values = []
+        self.__actual_perimeter_values = []
         self.__counter = 0
         self.__max_values = 10
         self.alarm_status = False
         self.__event = event_handler
+        self.__lowest_diff_percentage = 0.5
+        self.__highest_diff_percentage = 1.5
 
     def analyze_data(self, sensor1_data):
-        if not self.__sensor1:
+        if not self.__perimeter:
             if self.__counter <= self.__max_values:
                 if sensor1_data is not None:
-                    self.__sensor1_values.append(self.__turn_to_distance(sensor1_data))
+                    self.__perimeter_values.append(self.__turn_to_distance(sensor1_data))
                     self.__counter += 1
 
             if self.__counter == self.__max_values:
-                self.__sensor1 = int((sum(self.__sensor1_values))/self.__max_values)
+                self.__perimeter = int((sum(self.__perimeter_values)) / self.__max_values)
                 self.__counter = 0
-                self.__actual_sensor1 = self.__sensor1
+                self.__actual_perimeter = self.__perimeter
 
         else:
             if self.__counter <= self.__max_values:
-                self.__actual_sensor1_values.append(self.__turn_to_distance(sensor1_data))
+                self.__actual_perimeter_values.append(self.__turn_to_distance(sensor1_data))
                 self.__counter += 1
 
             if self.__counter == self.__max_values and not self.alarm_status:
-                self.__actual_sensor1 = 0
-                self.__actual_sensor1 = int((sum(self.__actual_sensor1_values) / self.__max_values))
-                self.__actual_sensor1_values = []
+                self.__actual_perimeter = 0
+                self.__actual_perimeter = int((sum(self.__actual_perimeter_values) / self.__max_values))
+                self.__actual_perimeter_values = []
                 self.__counter = 0
 
-            if self.__actual_sensor1 is None:
+            if self.__actual_perimeter is None:
                 return
 
-            if not (self.__sensor1 * .5 <= self.__actual_sensor1 <= self.__sensor1 * 1.5) and not self.alarm_status:
+            if not (self.__perimeter * self.__lowest_diff_percentage <= self.__actual_perimeter <= self.__perimeter * self.__highest_diff_percentage) and not self.alarm_status:
                 self.__event(caller='VOICE', event='ALARM', action='SET', status=True)
 
     def __turn_to_distance(self, data):
@@ -54,7 +56,7 @@ class AlarmManager():
     def set_alarm(self, status):
         self.alarm_status = status
         if not status:
-            self.__sensor1 = None
-            self.__sensor1_values = []
-            self.__actual_sensor1_values = []
+            self.__perimeter = None
+            self.__perimeter_values = []
+            self.__actual_perimeter_values = []
             self.__counter = 0
