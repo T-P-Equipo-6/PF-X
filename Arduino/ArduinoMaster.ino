c2042282a364a72600de96d901be9bc3e912a962
Bernardo Domínguez <unop_77@MacBook-Pro-de-Alberto-Dominguez.local>
2017-11-30 23:30:03 -0600

    Added door system

--- Arduino/ArduinoMaster.ino
+++ Arduino/ArduinoMaster.ino
@@ -1,63 +1,68 @@
 #include <Wire.h>
 
-int UltrasonicSensor1Out = 2;
-int UltrasonicSensor1In = 3;
-int UltrasonicSensor2Out = 4;
-int UltrasonicSensor2In = 5;
+int UltrasonicSensor1Out = 7;
+int UltrasonicSensor1In = 8;
+
+int motorForwardPin = 9;
+int motorBackwardPin = 10;
+
+int bedroom = 2;
+int studio = 3;
+int kitchen = 4;
+int bathroom = 5;
 int fan = 6;
-int bedroom = 10;
-int kitchen = 11;
-int livingroom = 12;
-int studio = 13;
+
 int TempSensorPin = A0;
 
 int state = 0;
 
+bool isOpen = false;
+
 String inCommand = "";
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   
   pinMode(bedroom, OUTPUT);
   pinMode(kitchen, OUTPUT);
-  pinMode(livingroom, OUTPUT);
+  pinMode(bathroom, OUTPUT);
   pinMode(studio, OUTPUT);
   pinMode(fan, OUTPUT);
+  
   pinMode(UltrasonicSensor1Out, OUTPUT);
   pinMode(UltrasonicSensor1In, INPUT);
-  pinMode(UltrasonicSensor2Out, OUTPUT);
-  pinMode(UltrasonicSensor2In, INPUT);
+
+  pinMode(motorForwardPin, OUTPUT);
+  pinMode(motorBackwardPin, OUTPUT);
+  
   digitalWrite(UltrasonicSensor1Out, LOW);
   digitalWrite(UltrasonicSensor1In, LOW);
-  digitalWrite(UltrasonicSensor2Out, LOW);
-  digitalWrite(UltrasonicSensor2In, LOW);
+  
   digitalWrite(bedroom, LOW);
   digitalWrite(kitchen, LOW);
-  digitalWrite(livingroom, LOW);
+  digitalWrite(bathroom, LOW);
   digitalWrite(studio, LOW);
   digitalWrite(fan, LOW);
+
+  digitalWrite(motorForwardPin, LOW);
+  digitalWrite(motorBackwardPin, LOW);
   
   Wire.begin();
 }
 
 void loop() {
   float UltrasonicSensorValue1 = readUltrasonicSensorValue(UltrasonicSensor1Out, UltrasonicSensor1In);
-  float UltrasonicSensorValue2 = readUltrasonicSensorValue(UltrasonicSensor2Out, UltrasonicSensor2In);
   float TempSensorValue = readTempSensorValue();
-  sendData(UltrasonicSensorValue1, UltrasonicSensorValue2, TempSensorValue);
+  sendData(UltrasonicSensorValue1, TempSensorValue);
 }
 
-void sendData(float UltrasonicSensorValue1, float UltrasonicSensorValue2, float TempSensorValue) {
-  Serial.print("{\"UltrasonicSensorValue1\": ");
-  Serial.print("\"UltrasonicSensorValue1\"");
+void sendData(float UltrasonicSensorValue1, float temperatureValue) {
+  Serial.print("{\"USV\": ");
+  Serial.print(UltrasonicSensorValue1);
   Serial.print(", ");
-  Serial.print("\"UltrasonicSensorValue2\": ");
-  Serial.print("\"UltrasonicSensorValue2\"");
-  Serial.print(", ");
-  Serial.print("\"TempSensorValue\": ");
-  Serial.print("\"TempSensorValue\"");
+  Serial.print("\"TEMP\": ");
+  Serial.print(temperatureValue);
   Serial.println("}");
-  delay(100);
 }
 
 float readUltrasonicSensorValue(int Out, int In) {
@@ -94,11 +99,11 @@ void serialEvent() {
   if (inCommand == "LIGHTSKITCHENOFF"){
     digitalWrite(kitchen, LOW);
   }
-  if (inCommand == "LIGHTSLIVINGROOMON"){
-    digitalWrite(livingroom, HIGH);
+  if (inCommand == "LIGHTSBATHROOMON"){
+    digitalWrite(bathroom, HIGH);
   }
-  if (inCommand == "LIGHTSLIVINGROOMOFF"){
-    digitalWrite(livingroom, LOW);
+  if (inCommand == "LIGHTSBATHROOMOFF"){
+    digitalWrite(bathroom, LOW);
   }
   if (inCommand == "FANHOUSEON"){
     digitalWrite(fan, HIGH);
@@ -106,12 +111,32 @@ void serialEvent() {
   if (inCommand == "FANHOUSEOFF"){
     digitalWrite(fan, LOW);
   }
+  if (inCommand == "DOORFRONTOPEN"){
+    if (isOpen == false){
+      digitalWrite(motorForwardPin, HIGH);
+      digitalWrite(motorBackwardPin, LOW);
+      delay(2000);
+      digitalWrite(motorForwardPin, LOW);
+      digitalWrite(motorBackwardPin, LOW);
+      isOpen = true;
+    }
+  }
+  if (inCommand == "DOORFRONTCLOSE"){
+    if (isOpen = true){
+      digitalWrite(motorForwardPin, LOW);
+      digitalWrite(motorBackwardPin, HIGH);
+      delay(2000);
+      digitalWrite(motorForwardPin, LOW);
+      digitalWrite(motorBackwardPin, LOW);
+      isOpen = false;
+    }
+  }
   if (inCommand == "ALARMON"){
     state = 1;
 
     digitalWrite(bedroom, LOW);
     digitalWrite(kitchen, LOW);
-    digitalWrite(livingroom, LOW);
+    digitalWrite(bathroom, LOW);
     digitalWrite(studio, LOW);
     Wire.beginTransmission(1);
     Wire.write(state);
@@ -125,4 +150,3 @@ void serialEvent() {
     Wire.endTransmission();
   }
 }
-
